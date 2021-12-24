ae64b8d38bfe97c65f829e3c4aeaa383f8a1c8ef
aakash ojha <43116838+aakashojha22@users.noreply.github.com>
2021-12-24 22:27:46 +0530

    1

--- freJun_api/settings.py
+++ freJun_api/settings.py
@@ -81,14 +81,13 @@ WSGI_APPLICATION = 'freJun_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-
+"""
 DATABASES = {
 
     'default': {
@@ -108,7 +107,7 @@ DATABASES = {
     }
 
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
