605bbc985f80ea6d896aa87e23ac12ee04a374fe
aakash ojha <43116838+aakashojha22@users.noreply.github.com>
2021-12-24 23:01:34 +0530

    45

--- freJun_api/settings.py
+++ freJun_api/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
